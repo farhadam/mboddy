99c8b4213153f92170894165bf0ec835ad68d566
Farhad Aghaie Mibody <farhad.mibody@gmail.com>
2013-09-01 11:34:36 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mboddy::Application.config.secret_key_base = '1e7126b27f9826335fd53b67afff1575e04aed8b9b6465069ddd727abddd87b14a77f725d869196b9ed10fd23755932f8ae2b8dca734ceadd6d5540cc0eb1055'
