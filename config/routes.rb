321301c04d23f2ee77937c05619398cf4cff7ebf
Farhad Aghaie Mibody <farhad.mibody@gmail.com>
2013-09-02 23:17:50 -0400

    few change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Mboddy::Application.routes.draw do
-
+  root  'home#index'
   resources :microposts
 
   resources :users
+  match '/signup', to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
